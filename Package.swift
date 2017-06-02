ee086b1f9c434788487116ed89b74a8ad03c9a03
Adam Sharp <adam@sharplet.me>
2017-06-02 16:01:36 -0400

    Update minimum Swift version to 3.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,10 @@
+// swift-tools-version:3.1
+
 import PackageDescription
 
 let package = Package(
     name: "Nimble",
+    swiftLanguageVersions: [3],
     exclude: [
       "Sources/Lib",
       "Sources/NimbleObjectiveC",
